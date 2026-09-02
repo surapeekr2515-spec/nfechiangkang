a2fb342232cae50992bc40f1d45a9f69ec7514c8
surapeekr2515-spec <surapee.kr2515@gmail.com>
2026-09-02 11:44:40 +0700

    Update profile-config.js

--- profile-config.js
+++ profile-config.js
@@ -1,2 +1,2 @@
 window.STUDENT_PROFILE_WEB_APP_URL =
-  'https://script.google.com/macros/s/AKfycbzZPKCjqrvptWM7nVyquVLeU2GlKrS2UtljX1vQCqDHR2UVsL_-Tyh5BqVctKUwsv1C/exec';
+  'https://script.google.com/macros/s/AKfycbzSV7SkUhXLTUDTAjY6qOkB6LVKFZFKPrSAqwBQ0iO9Z48oFZLjE2en5jnM4x4Wa77Z/exec';
